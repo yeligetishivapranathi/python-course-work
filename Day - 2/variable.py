7cd507851aea87e3bbe002c2aabb7dccf58cfadf
yeligetishivapranathi <yeligetishivapranathi@gmail.com>
2026-05-27 17:49:00 +0530

    Add files via upload

---
+++ Day - 2/variable.py
@@ -0,0 +1,32 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================================================== RESTART: C:/Users/Shiva kiran/OneDrive/ysp pro/python course/Day - 2/keywords.py ===================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> Myvar=10
+>>> myvar1=10
+>>> MyVar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_var=10
+>>> my var=10
+SyntaxError: invalid syntax
+>>> myvar=10
+>>> var=20
+>>> -20=20
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> _20=20
+>>> var=10
+>>> Var=20
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> b
+20
+>>> c
+30
+>>> a,b=b,a
+>>> a
+20
+>>> b
+10
